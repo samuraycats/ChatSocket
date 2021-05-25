8e46614759d17f8007a1f8ed2797b762e871ae84
lorenzo.rascon <lorenzo.rascon@beekmovil.com>
2021-05-25 13:06:49 -0500

    uso de post es el back

--- back/src/app.js
+++ back/src/app.js
@@ -1,21 +1,39 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const socket = require(`${process.env.SRC}socket`);
 
 const app = express();
 
-const corsOptions = {
-    origin: process.env.FRONT_ENDPOINT,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+let whiteList = [{url:process.env.FRONT_ENDPOINT, status:true}];
 
-app.get("/", cors(corsOptions), (rep, resp, next) => {
-    resp.send({ id: 0, nombre: "Lorenzo" })
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.some( e => e.url === origin && e.status === true )) {
+      console.log("ok");
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+
+  }
+}
+
+app.get("/prueba", cors(corsOptions), (req, resp, next) => {
+  resp.send({ id: 0, nombre: "Lorenzo" })
+});
+
+app.post("/user", cors(corsOptions), (req, resp, next) => {
+  console.clear();
+  console.log(req.body.user);
+
+  resp.send({ req: "" })
 });
 
 const http = require("http").createServer(app);
