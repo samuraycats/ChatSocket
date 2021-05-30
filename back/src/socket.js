4e0d1560fe3f40959d75b32b5c4cf827ee0a77b5
lorenzo.rascon <lorenzo.rascon@beekmovil.com>
2021-05-30 18:16:29 -0500

    caambio en el socket del server

--- back/src/socket.js
+++ back/src/socket.js
@@ -1,65 +1,91 @@
 const dotenv = require('dotenv').config();
-const mysql = require('mysql');
+
+const UserService = require('./services/UserService');
+const MessageService = require('./services/MessageService');
 
 module.exports = (http) => {
+
     const io = require('socket.io')(http, {
         cors: {
             origin: process.env.FRONT_CHAT,
             methods: ["GET", "POST", "PUT", "DELETE"]
         }
     });
-
-    const dataBaseConexion = {
-        host: process.env.HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_DATABASE
-    };
-
     let messages = [];
     let users = [];
+
+    if (messages.length === 0) {
+        const messageService = new MessageService();
+
+        messageService.get()
+            .then((response) => {
+                messages.push(response.data);
+            })
+            .catch((error) => {
+                console.log("Error al recuperar datos")
+            })
+            .finally(() => {
+                io.emit('messages', messages);
+            })
+    }
 
     io.on('connection', (socket) => {
 
         socket.on("new-user", (data) => {
-            const con = mysql.createConnection(dataBaseConexion)
-            con.connect();
-
             let user = { id: null, user: data.user, date: new Date(), socket: socket.id, status: true, messages: 0 };
+            const userService = new UserService(user);
 
-            con.query('INSERT INTO user SET ?', { user: user.user, date: user.date, socket: socket.id, status: true }, (error, results, fields) => {
-                if (error) throw err;
-                user.id = results.insertId;
-                con.end();
-
-                users.push(user);
-                io.emit("users", users)
-            });
-        });
-
-        socket.on("new-message", function (data) {
-            const con = mysql.createConnection(dataBaseConexion)
-            con.connect();
-            let newMessage = {id: data.id, from:data.from, message:data.message, to:data.to, date:data.date}
-        
-            if (newMessage.message !== ""){
-                con.query('INSERT INTO message SET ?', newMessage, (error, results, fields) => {
-                    data.id = results.insertId;
-                    con.end();
-    
-                    messages.push(data);
-                    io.emit('messages', messages);
+            userService.post(user)
+                .then((response) => {
+                    if (response.data.id > 0) {
+                        users.push(response.data);
+                    }
+                })
+                .catch((error) => {
+                    console.log("Fallo al crear el usuario");
+                })
+                .finally(() => {
+                    io.emit("users", users)
                 });
-            } 
         });
 
-        socket.on('disconnect', function () {
-            const con = mysql.createConnection(dataBaseConexion)
-            con.connect();
+        socket.on("new-message", (data) => {
 
-            con.query('UPDATE user SET status = false WHERE socket = ? ', [socket.id], (error, result) => {
-                users = users.filter(e => (e.socket !== socket.id));
-                io.emit("users", users)
-            });
+            let newMessage = { id: null, from: data.from, message: data.message, to: data.to, date: data.date }
 
-            console.log('has disconnected from the chat.');
+            if (newMessage.message !== "") {
+                const messageService = new MessageService();
+
+                messageService.post(newMessage)
+                    .then((response) => {
+                        if (response.data.id > 0) {
+                            messages.push(response.data);
+                        }
+                    })
+                    .catch(() => {
+                        console.log("Falló al insertar en mensaje ")
+                    })
+                    .finally(() => {
+                        io.emit('messages', messages);
+                    })
+            }
+        });
+
+        socket.on('disconnect', () => {
+
+            const userService = new UserService();
+
+            userService.putSocket({ status: false }, socket.id)
+                .then((response) => {
+                    users = users.filter(e => (e.socket !== socket.id));
+                    io.emit("users", users)
+                })
+                .catch(() => {
+                    console.log('Error al actualizar el status del socket');
+                })
+                .finally(() => {
+                    io.emit("users", users)
+                });
         });
 
         socket.emit('users', users);
